4e96e8a7d90861813387c17ca16c702ef4cf271a
hadis momeni <hadismraad@gmail.com>
2025-01-09 11:14:48 +0330

    create overall error file

---
+++ src/app/error.js
@@ -0,0 +1,5 @@
+"use client";
+
+export default function ErrorBoundary() {
+  return <div className='w-full h-full flex items-center justify-center font-bold bg-pink-800 text-amber-50 text-4xl'>Error in application.please refresh</div>;
+}
