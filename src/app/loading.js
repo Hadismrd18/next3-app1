25adaf78e9143a3aa43c40bcf6f04ec4577152be
hadis momeni <hadismraad@gmail.com>
2025-01-09 11:15:12 +0330

    create overall loading file

---
+++ src/app/loading.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function loading() {
+  return (
+    <div className="w-full h-full flex items-center justify-center font-bold text-pink-800 text-4xl">
+      Loading...
+    </div>
+  );
+}
