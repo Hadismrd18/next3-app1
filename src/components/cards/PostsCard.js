faf5074dc703ab4fa57452329845876166e1a3ad
hadis momeni <hadismraad@gmail.com>
2025-01-17 14:29:07 +0330

    move cards to another folder

--- src/components/cards/PostsCard.js
+++ src/components/cards/PostsCard.js
@@ -1,30 +1,28 @@
 // Card.jsx
 "use client"; // Designating Card as a client component
 import Link from "next/link";
-import React, { useEffect } from "react";
+import React from "react";
 import { FaEye } from "react-icons/fa6";
-export default async function Card({ cardData }) {
-  const { id, name, instructions, prepTimeMinutes, rating } = cardData;
-  await new Promise((resolve) => setTimeout(resolve, 3000));
+export default function Card({ cardData }) {
+  const { _id, title, body, userId, views } = cardData;
+  console.log(_id, title, body, userId, views);
+  
   return (
     <div
-      key={id}
+      key={_id}
       className="p-4 hover:bg-amber-300 hover:scale-105 transition-all duration-200 bg-amber-200 rounded py-6 flex flex-col items-center justify-between"
     >
-      <h1 className="font-semibold text-fuchsia-900">{name}</h1>
-      <p className="font-bold text-pink-700">{instructions}</p>
+      <h1 className="font-semibold text-fuchsia-900">{title}</h1>
+      <p className="font-bold text-pink-700">{body}</p>
       <div className="w-full flex flex-col items-center gap-2">
-        <div className="w-full bg-pink-900 flex items-center justify-around rounded p-1">
-          <span className="font-bold text-white ">
-            prepTimeMinutes: {prepTimeMinutes}
-          </span>
+        <div className="w-full bg-pink-900 flex flex-col items-start justify-center rounded p-1">
           <div className="flex items-center justify-center gap-1">
             <FaEye className="text-white" />
-            <span className="font-bold text-white ">rating: {rating}</span>
+            <span className="font-bold text-white ">views: {views}</span>
           </div>
         </div>
         <Link
-          href={`/recipes/${id}`}
+          href={`/posts/${_id}`}
           className="w-full text-center bg-purple-900 text-amber-50 rounded px-3 py-1"
         >
           More...
--- src/components/cards/UsersCard.js
+++ src/components/cards/UsersCard.js
@@ -4,20 +4,22 @@ import Link from "next/link";
 import React from "react";
 
 export default function Card({ cardData }) {
-  const { id, firstName, lastName, age } = cardData;
+  const { _id, firstName, lastName, age } = cardData;
 
   return (
     <div
-      key={id}
+      key={_id}
       className="hover:bg-amber-300 hover:scale-105 transition-all duration-200 bg-amber-200 rounded py-6 flex flex-col items-center justify-between"
     >
       <h1 className="font-semibold text-fuchsia-900">
-        {firstName} {lastName}
+        first name: {firstName}
+      </h1>
+      <h1 className="font-semibold text-fuchsia-900">
+        last name:  {lastName}
       </h1>
       <h2 className="font-bold text-pink-700">age: {age}</h2>
-      <span className="font-bold text-pink-700">id: {id}</span>
       <Link
-        href={`/users/${id}`}
+        href={`/users/${_id}`}
         className="bg-purple-900 text-amber-50 rounded px-3 py-1"
       >
         More...
